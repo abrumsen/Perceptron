c6155fc13cd76834aec19de603b6583604866d8f
A.Brumsen <a.brumsen@protonmail.com>
2025-02-27 06:30:43 +0100

    Docstrings & refactoring

--- perceptron/perceptron_base.py
+++ perceptron/perceptron_base.py
@@ -1,20 +1,10 @@
 import numpy as np
-import pandas as pd
 
 class Perceptron:
-    def __init__(self, input_size: int, learning_rate: float=0.3, epochs: int=1000) -> None:
+    def __init__(self, input_size: int, learning_rate: float = 0.3, epochs: int = 1000) -> None:
         if type(self) is Perceptron:
             raise RuntimeError("Perceptron is an abstract class and cannot be instantiated directly")
         self.input_size = input_size
         self.learning_rate = learning_rate
         self.epochs = epochs
         self.weights = np.random.normal(loc=0.0, scale=1.0, size=input_size + 1)
-
-    def activation_function(self, potential: float):
-        raise NotImplementedError()
-
-    def predict(self, inputs: np.ndarray):
-        raise NotImplementedError()
-
-    def train(self, training_data: pd.DataFrame) -> None:
-        raise NotImplementedError()
--- perceptron/perceptron_simple.py
+++ perceptron/perceptron_simple.py
@@ -3,29 +3,53 @@ import pandas as pd
 
 from perceptron.perceptron_base import Perceptron
 
+
 class PerceptronSimple(Perceptron):
-    def __init__(self, input_size: int, learning_rate: float=0.3, epochs: int=1000) -> None:
+    def __init__(self, input_size: int, learning_rate: float = 0.3, epochs: int = 1000) -> None:
         super().__init__(input_size, learning_rate, epochs)
 
-    def activation_function(self, potential: float) -> int:
+    @staticmethod
+    def activation_function(potential: float) -> int:
+        """
+        Defines the activation function of the perceptron.
+        :param potential: The calculated potential.
+        :return: 1 if activated, 0 if not.
+        """
         return 1 if potential > 0 else 0
 
     def predict(self, inputs: np.ndarray) -> int:
+        """
+        Calculates the prediction of the perceptron.
+        :param inputs: The input vector (includes bias).
+        :return: 1 if activated, 0 if not.
+        """
         return self.activation_function(np.dot(self.weights, inputs))
 
     def correct(self, error: float, x: np.ndarray) -> None:
+        """
+        Correct synaptic weights using the basic perceptron rule.
+        :param error: The difference between the predicted and expected value.
+        :param x: The input vector (includes bias).
+        """
         for i in range(len(x)):
             self.weights[i] += self.learning_rate * error * x[i]
 
-    def train(self, training_data: pd.DataFrame) -> None:
+    def train(self, training_data: pd.DataFrame) -> int:
+        """
+        Trains the perceptron with the given training data using the basic perceptron algorithm.
+        :param training_data: The training data dataframe.
+        :return: 1 if training was successful, 0 if not.
+        """
         for epoch in range(self.epochs):
             errors = 0
-            for index, row in training_data.iterrows():
+            for _, row in training_data.iterrows():
                 prediction = self.predict(row["inputs"])
                 error = row["label"] - prediction
                 if error != 0:
                     errors += 1
                     self.correct(error, row["inputs"])
-            if not errors:
-                print(f"Training complete for {epoch} epochs")
-                break
+            if errors == 0:
+                print(f"Training complete for {epoch + 1} epochs")
+                return 1
+        print(f"Training stopped after {self.epochs} epochs")
+        return 0
